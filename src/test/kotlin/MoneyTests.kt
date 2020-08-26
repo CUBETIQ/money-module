3f02799284836db9a92df24890154fcfc531a1cc
Sambo Chea <sombochea@cubetiqs.com>
2020-08-26 21:48:03 +0700

    Add tests for money exchange

--- src/test/kotlin/MoneyTests.kt
+++ src/test/kotlin/MoneyTests.kt
@@ -1,5 +1,6 @@
 import com.cubetiqs.libra.moneyutils.Money
 import com.cubetiqs.libra.moneyutils.MoneyConfig
+import com.cubetiqs.libra.moneyutils.MoneyExchangeUtils
 import com.cubetiqs.libra.moneyutils.plus
 import com.cubetiqs.libra.moneyutils.times
 import com.cubetiqs.libra.moneyutils.timesAssign
@@ -27,5 +28,10 @@ class MoneyTests {
             .parse("USD:1,KHR:4000")
 
         println(MoneyConfig.getConfig())
+
+        val moneyUsd = Money(10.0)
+        val moneyKhr = MoneyExchangeUtils.exchange(moneyUsd, "KHR")
+
+        Assert.assertEquals(40000.0, moneyKhr.getMoneyValue(), 0.0)
     }
 }
