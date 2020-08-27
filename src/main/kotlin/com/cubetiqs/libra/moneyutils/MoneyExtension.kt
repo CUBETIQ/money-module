4dc6ae521c17d97e9aaed65f0911476dd9265928
Sambo Chea <sombochea@cubetiqs.com>
2020-08-27 10:48:31 +0700

    Add money extension and operator

---
+++ src/main/kotlin/com/cubetiqs/libra/moneyutils/MoneyExtension.kt
@@ -0,0 +1,3 @@
+package com.cubetiqs.libra.moneyutils
+
+fun StdMoney
--- src/main/kotlin/com/cubetiqs/libra/moneyutils/MoneyOperator.kt
+++ src/main/kotlin/com/cubetiqs/libra/moneyutils/MoneyOperator.kt
@@ -1,13 +1,12 @@
 package com.cubetiqs.libra.moneyutils
 
-operator fun Money.unaryMinus() = (-getMoneyValue())
-operator fun Money.unaryPlus() = (+getMoneyValue())
+operator fun StdMoney.unaryMinus() = (-getMoneyValue())
+operator fun StdMoney.unaryPlus() = (+getMoneyValue())
 operator fun Money.inc() = Money(value++)
 operator fun Money.dec() = Money(value--)
-operator fun Money.plus(other: Money) = Money(value + other.value)
-operator fun Money.times(other: Money) = Money(value * other.value)
-operator fun Money.div(other: Money) = Money(value / other.value)
-operator fun Money.timesAssign(other: Money) {
-    this.value = this.value * other.value
+operator fun StdMoney.plus(other: StdMoney) = Money(getMoneyValue() + other.getMoneyValue())
+operator fun StdMoney.times(other: StdMoney) = Money(getMoneyValue() * other.getMoneyValue())
+operator fun StdMoney.div(other: StdMoney) = Money(getMoneyValue() / other.getMoneyValue())
+operator fun Money.timesAssign(other: StdMoney) {
+    this.value = this.getMoneyValue() * other.getMoneyValue()
 }
-operator fun Money.not() = this.value != this.value
