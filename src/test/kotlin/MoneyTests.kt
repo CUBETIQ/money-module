7204246dc9bef617b63afa74c4a71ffd99331f18
Sambo Chea <sombochea@cubetiqs.com>
2020-08-27 08:46:33 +0700

    Add tests for money

--- src/test/kotlin/MoneyTests.kt
+++ src/test/kotlin/MoneyTests.kt
@@ -9,13 +9,16 @@ import org.junit.Test
 
 class MoneyTests {
     @Test
-    fun test() {
+    fun money_operator_test() {
         val money = Money(10.0)
         val money2 = Money(20.0)
         money *= money
         println((money + money2) * money2)
-        Assert.assertEquals(10, 10)
+        Assert.assertEquals(100.0, money.value, 0.0)
+    }
 
+    @Test
+    fun exchange_2usd_to_khr_test() {
         val properties = MoneyConfig
             .MoneyConfigProperties
             .MoneyConfigPropertiesBuilder()
@@ -27,11 +30,13 @@ class MoneyTests {
             .setProperties(properties)
             .parse("USD:1,KHR:4000")
 
+        Assert.assertTrue(MoneyConfig.isValid())
+
         println(MoneyConfig.getConfig())
 
-        val moneyUsd = Money(10.0)
+        val moneyUsd = Money(2.0)
         val moneyKhr = MoneyExchangeUtils.exchange(moneyUsd, "KHR")
 
-        Assert.assertEquals(40000.0, moneyKhr.getMoneyValue(), 0.0)
+        Assert.assertEquals(8000.0, moneyKhr.getMoneyValue(), 0.0)
     }
 }
